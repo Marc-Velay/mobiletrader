a7d36e7c7569418399cffee0b66aa523c5072ce2
Marc-Velay <marc.velay@gmail.com>
2017-12-30 14:23:00 +0100

    added dashboard components

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -1,27 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
-/**
- * Generated class for the DashboardComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+import { NavController } from 'ionic-angular';
+
+import { Item } from '../../providers/item/item-model';
+import { ItemProvider } from '../../providers/item/item';
+
+import { ItemDetailsPage } from '../../pages/item-details/item-details';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'dashboard.html',
   styleUrls: ['/src/components/dashboard/dashboard.css']
 })
 export class DashboardComponent implements OnInit {
+  items: Item[] = [];
+  topItems: Item[] = [];
 
-  text: string;
+  constructor(
+        private itemService: ItemProvider,
+        public navCtrl: NavController
+  ) { }
 
-  constructor() {
-    console.log('Hello DashboardComponent Component');
-    this.text = 'Hello World';
-  }
-  
+  //Load items on page load
   ngOnInit() {
+    this.getItems();
+  }
 
+  getItems(): void {
+    this.itemService.getItems()
+      .subscribe(items => {
+          this.items = items;
+          this.topItems = items.slice(0, 5); //Keep the first 5 items as the "top" items
+      });
+  }
+
+  openPage(itemID:any){
+    // push another page onto the navigation stack
+    // causing the nav controller to transition to the new page
+    // optional data can also be passed to the pushed page.
+    this.navCtrl.push(ItemDetailsPage, {
+      id: itemID
+    });
   }
 
 }
